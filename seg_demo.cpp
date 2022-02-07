b8e0f78298bfc9f67e27aa5ee4c2f53e95a429dc
xiongzhu666 <52955431+xiongzhu666@users.noreply.github.com>
2022-02-07 12:47:22 +0800

    Update seg_demo.cpp

--- seg_demo.cpp
+++ seg_demo.cpp
@@ -14,12 +14,12 @@ int main(int argc, char** argv)
     skynet.load_model("../skysegsmall_sim-opt-fp16.bin");
     cv::Mat bgr = cv::imread("../eval/test_800.jpg", 1);
     cv::Mat dst = bgr.clone();
-    while (dst.rows >640 && dst.cols >640 ) {
+    while (dst.rows > 768 && dst.cols >768 ) {
         pyrDown(dst, dst, cv::Size(dst.cols / 2, dst.rows / 2));
     }
     int w = bgr.cols;
     int h = bgr.rows;
-    ncnn::Mat in = ncnn::Mat::from_pixels_resize(dst.data, ncnn::Mat::PIXEL_BGR2RGB, dst.cols, dst.rows, 320, 320);
+    ncnn::Mat in = ncnn::Mat::from_pixels_resize(dst.data, ncnn::Mat::PIXEL_BGR2RGB, dst.cols, dst.rows, 384, 384);
     const float mean_vals[3] =  {0.485f*255.f, 0.456f*255.f, 0.406f*255.f};
     const float norm_vals[3] = {1/0.229f/255.f, 1/0.224f/255.f, 1/0.225f/255.f};
     in.substract_mean_normalize(mean_vals, norm_vals);
